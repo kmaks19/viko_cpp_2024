eb4f2aafbc0930642dd4c8054673b59700ebbb08
Karolis <46166663+kmaks19@users.noreply.github.com>
2024-11-03 11:05:40 +0200

    Current

--- Praktiniai_Darbai/Praktinis_Darbas_3/main.cpp
+++ Praktiniai_Darbai/Praktinis_Darbas_3/main.cpp
@@ -8,70 +8,81 @@ constexpr int Default_Array_Size = 30;
 
 const char LT_ABC_ARRAY[32] = // Abëcëlë saugojama masyve.
 {
-    'A', 'À', 'B', 'C', 'È', 'D', 'E', 'Æ', 'Ë', 'F', 'G', 'H', 'I', 'Á', 'Y', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'Ð', 'T', 'U', 'Ø', 'Û', 'V', 'Z', 'Þ'
+    'A', 'À', 'B', 'C', 'È', 'D', 'E', 'Æ', 'Ë', 'F', 'G', 'H', 'I', 'Á', 'Y', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R',
+    'S', 'Ð', 'T', 'U', 'Ø', 'Û', 'V', 'Z', 'Þ'
 };
 
 const char ASCII_ALPHABET_ARRAY[96] =
 {
-    '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', \
-    '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', \
-    ':', ';', '<', '=', '>', '?', '@', \
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', \
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', \
-    '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', \
+    '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-',
+    '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ':', ';', '<', '=', '>', '?', '@',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^',
+    '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'
 };
 
 int main(){
     system("chcp 1257");
 
-    while (true)
+    while(true)
     {
-        cout << "Ðifravimo/Deðifravimo sistema\nPasirinkite: \n1. Ðifruoti tekstà\n2. Deðifruoti tekstà\n";
+        cout << "Ðifravimo/Deðifravimo sistema\n";
+        cout << "» Pasirinkite: \n1 - Uþðifruoti tekstà\n2 - Deðifruoti tekstà\n» ÁVEDIMAS: ";
 
-        int menu_option;
-        cin >> menu_option;
+        int choice;
+        cin >> choice;
 
-        if (cin.fail() || menu_option < 1 || menu_option > 2)
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "[KLAIDA]: Pasirinkti privalote tarp [1-2] !\n";
+        if(choice > 2 || choice < 1){ // TODO: ADD cin.fail, cin.reset(), UPGRADE
+            cout << "[KLAIDA]: Privalote pasirinkti [1-2] !\n";
             continue;
         }
 
-        int alphabet_option, strlen_alphabet;
-
         while(true)
         {
-            cout << "Pasirinkite abëcëlæ: \n1 - Lietuviø abëcëlë\n2 - ASCII koduotë\n";
+            cout << "» Pasirinkite: \n1 - LT abëcëlë\n2 - ASCII koduotë\n» ÁVEDIMAS: ";
 
-            cin >> alphabet_option;
+            int koduote, n;
 
-            if(cin.fail() | alphabet_option < 1 || alphabet_option > 2)
-            {
-                cout << "[KLAIDA]: Pasirinkti privalote tarp [1-2]!" << endl;
+            cin >> koduote;
+
+            if(koduote > 2 || koduote < 1){ // TODO: ADD cin.fail, cin.reset() UPGRADE
+                cout << "[KLAIDA]: Privalote pasirinkti tarp [1-2] !\n";
                 continue;
             }
 
-            switch (alphabet_option)
-            {
-                case 1: {
-                    strlen_alphabet = sizeof(LT_ABC_ARRAY) / sizeof(LT_ABC_ARRAY[0]);
+            switch(koduote){
+                case 1:{
+                    n = sizeof(LT_ABC_ARRAY) / sizeof(LT_ABC_ARRAY[0]);
                     break;
                 }
-                case 2: {
-                    strlen_alphabet = sizeof(ASCII_ALPHABET_ARRAY) / sizeof(ASCII_ALPHABET_ARRAY[0]);
+                case 2:{
+                    n = sizeof(ASCII_ALPHABET_ARRAY) / sizeof(ASCII_ALPHABET_ARRAY[0]);
                     break;
                 }
-                default: {
-                    cout << "[KLAIDA]: SWITCH: DEFAULT FAULT \n";
-                    return 0;
+                default:{
+                    cout << "[KLAIDA]: SWITCH DEFAULT FAULT\n";
+                    break;
                 }
             }
             break;
         }
+
+        char P[50];
+
+        while(true){
+            cout << "» Áveskite pradiná tekstà:\n\t• Pradinis tekstas negali bûti ilgesnis nei 25 simboliai\n\t\• Negali bûti trumpesnis nei 1 simbolis\n\t• Gali bûti sudarytas tik ið abëcëlëje esanèiø simboliø\n» ÁVEDIMAS: ";
+
+            cin >> P;
+
+
+
+            break;
+        }
+
         break;
     }
+
     return 0;
 }
